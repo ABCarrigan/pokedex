8f57c90b1468cfdcee34f7aa7d551ea9c01bc2ba
Addison Carrigan <abcarrigan@gmail.com>
2022-02-28 03:31:51 -0500

    added files

---
+++ README.md
@@ -0,0 +1,2 @@
+Enter a Pokemon's ID Number and the site will pull the corresponding Pokemon's information from PokeAPI.
+<ahref="https://pokeapi.co/">Link to the PokeAPI's main page</a>
---
+++ app.js
@@ -0,0 +1,27 @@
+document.getElementById('pokeSubmit').onclick = function() {pokeUserSubmission()}; 
+function pokeUserSubmission() {
+    let pokeQuery = document.getElementById("userPokeNum").value;
+    let pokeApiLink = `https://pokeapi.co/api/v2/pokemon/${pokeQuery}`;
+    // https://pokeapi.co/api/v2/pokemon/1
+    // get the data from the API via URL
+    fetch(pokeApiLink)
+    // use .then to handle the response/reject promise
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data.name + data.id);
+        pokeName.innerHTML = data.name;
+        pokeNum.innerHTML = 'No. '+ data.id+'<sub>/898</sub>';
+        pokeWeight.innerHTML = 'Weight: '+data.weight;
+        pokeHeight.innerHTML = 'Height: '+data.height;
+        pokeSpriteFront.src = data.sprites.other.home.front_default;
+        pokeSpriteShiny.src = data.sprites.other.home.front_shiny;
+    })
+    var pokeName = document.getElementById('pokeName');
+    var pokeNum = document.getElementById('pokeNum');
+    var pokeWeight = document.getElementById('pokeWeight');
+    var pokeHeight = document.getElementById('pokeHeight');
+    var pokeSpriteFront = document.getElementById('pokeSpriteFront');
+    var pokeSpriteShiny = document.getElementById('pokeSpriteShiny');
+}
